6105f25f3951224a3e7242c8e9ec4483beb7c455
makarooney <j.harizaj@gmail.com>
2017-08-05 12:37:38 -0400

    sentenceOccurences, list mkstring

--- 5-forcomp/src/main/scala/forcomp/Anagrams.scala
+++ 5-forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -43,7 +43,7 @@ object Anagrams {
   }
 
   /** Converts a sentence into its character occurrence list. */
-  def sentenceOccurrences(s: Sentence): Occurrences = ???
+  def sentenceOccurrences(s: Sentence): Occurrences = wordOccurrences( new Word(s mkString ""))
 
   /** The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
    *  the words that have that occurrence count.
